6aa2a4b55f1b001e535d97b12a18b5872f2a17c9
Oscar Gonzalez Luna <osgol.87@gmail.com>
2026-07-27 12:23:05 -0600

    Ajustes de pruebas unitarias

--- OrderService/src/test/java/com/speedsneakers/orderservice/OrderserviceApplicationTests.java
+++ OrderService/src/test/java/com/speedsneakers/orderservice/OrderserviceApplicationTests.java
@@ -2,8 +2,19 @@ package com.speedsneakers.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:orders;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class OrderserviceApplicationTests {
 
 	@Test
--- ProductServiceElastic/src/test/java/com/speedsneakers/productserviceelastic/ProductserviceelasticApplicationTests.java
+++ ProductServiceElastic/src/test/java/com/speedsneakers/productserviceelastic/ProductserviceelasticApplicationTests.java
@@ -1,10 +1,27 @@
 package com.speedsneakers.productserviceelastic;
 
+import com.speedsneakers.productserviceelastic.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"elasticsearch.host=localhost:9200",
+		"elasticsearch.user=test",
+		"elasticsearch.password=test"
+})
 class ProductserviceelasticApplicationTests {
+
+	@MockBean
+	private ProductRepository productRepository;
+
+	@MockBean
+	private ElasticsearchOperations elasticsearchOperations;
 
 	@Test
 	void contextLoads() {
